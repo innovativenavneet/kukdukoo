3d1faa28d0b30660f4f18e546694544636b60290
Shashikant-6502 <Shashikant6502@gmail.com>
2025-08-21 23:49:58 +0530

    Updated the acheivement and partner sections

--- src/components/sections/AchievementsSection.jsx
+++ src/components/sections/AchievementsSection.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import achievementsData from '../../data/achievementsData.json';
+import star from '../../assets/images/star-header-icon.png';
+import '../../styling/AchievementsSection.css';
 function AchievementsSection() {
   return (
     <section className="achievements-section">
-      <h2>FESTIVALS ACHIEVEMENTS</h2>
+      <h2>
+        <img src={star} alt="Star Icon" className='star-icon'></img>
+        FESTIVALS ACHIEVEMENTS
+        <img src={star} alt=" Icon" className='star-icon'></img>
+        </h2>
       <div className="achievements-grid">
         {achievementsData.achievements.map((item, index) => (
           <div key={index} className="achievement-item">
-            <p>{item.value}</p>
+             {item.icon && (
+              <img
+                src={`src/assets/images/${item.icon}`}
+                className="achievement-icon"
+              />
+            )}
+            <p className='achievement-value'>{item.value}</p>
             <p>{item.label}</p>
           </div>
         ))}
--- src/components/sections/PartnersSection.jsx
+++ src/components/sections/PartnersSection.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import partnersData from '../../data/partnersData.json';
+import leftArm from '../../assets/images/left-arm.png';
+import rightArm from '../../assets/images/right-arm.png';
+import '../../styling/PartnerSection.css';
 function PartnersSection() {
   return (
     <section className="partners-section">
-      <h2>OUR PARTNERS</h2>
+      <h2>
+         <img src={leftArm} className='partner-icon'></img>
+         OUR PARTNERS
+          <img src={rightArm}  className='partner-icon'></img>
+      </h2>
       <div className="partners-grid">
         {partnersData.partners.map((partner, index) => (
-          <div key={index} className="partner-logo">
+          <div key={index} className="partner-card">
             <p>{partner.name}</p>
           </div>
         ))}
       </div>
+      
+      <button className="partner-btn">PARTNER WITH US →</button>
     </section>
   );
 }
